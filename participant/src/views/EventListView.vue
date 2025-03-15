313646d34396c68cc54c78462d899735ce29e38c
Zyler00 <chompoonoot_pimolrat@cmu.ac.th>
2025-03-15 19:20:32 +0700

    lab6

---
+++ participant/src/views/EventListView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import eventService from '@/services/EventService'
+import { ref } from 'vue'
+import type { Event } from '@/types'
+const events = ref<Event[]>([])
+  interface EventResponse {
+ data: Event[]
+}
+eventService.getEvents().then((response:EventRespose) => {
+ events.value = response.data
+})
+</script>
+
+<template>
+ <div>
+ <EventCard />
+ </div>
+</template>
+<style scoped>
+.events {
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+}
+</style>
